7e2d4e3ee65a773fdce10d67e0d032d8b03be236
Simon <s.lechner.biz@gmail.com>
2020-03-17 14:05:08 +0100

    Implemented getLivingNeighbourCount, fixed redundance with booleans, noticed code not working
    After implementing the suggested changes (twice), I noticed that the program doesn't work, it spits out the same array over and over again. Seems like I broke it before the last commit, was not able to find the reason.

--- src/com/codersbay/GameOfLife.java
+++ src/com/codersbay/GameOfLife.java
@@ -16,43 +16,29 @@ public class GameOfLife {
         return array;
     }
 
-    //Dead Cell
-    public static boolean DeadCell(int[][] array, int x, int y) {
-        if ((array[x][y] == 0) && ((array[x - 1][y - 1] + array[x - 1][y] + array[x - 1][y + 1]   //Dead Cell with 3 living neighbours
+    public static int getLivingNeighbourCount(int[][] array, int x, int y) {
+        return ((array[x - 1][y - 1] + array[x - 1][y] + array[x - 1][y + 1]
                 + array[x][y - 1] + array[x][y + 1]
-                + array[x + 1][y - 1] + array[x + 1][y] + array[x + 1][y + 1]) == 3)) {
-            return true;
-        } else {
-            return false;
-        }
+                + array[x + 1][y - 1] + array[x + 1][y] + array[x + 1][y + 1]));
+    }
+
+    //Dead Cell
+    public static boolean deadCell(int[][] array, int x, int y) {
+        return ((array[x][y] == 0) && (getLivingNeighbourCount(array, x, y) == 3));
     }
 
     //Lonely Cell
-    public static boolean LonelyCell(int[][] array, int x, int y) {
-        if (array[x][y] == 1 && (array[x - 1][y - 1] + array[x - 1][y] + array[x - 1][y + 1]   //Living cell &lt 2
-                + array[x][y - 1] + array[x][y + 1]
-                + array[x + 1][y - 1] + array[x + 1][y] + array[x + 1][y + 1]) < 2)
-            return true;
-        else {
-            return false;
-        }
+    public static boolean lonelyCell(int[][] array, int x, int y) {
+        return (array[x][y] == 1 && (getLivingNeighbourCount(array, x, y)) < 2);
     }
 
     //Happy Cell
-    public static boolean HappyCell(int[][] array, int x, int y) {
-        if (array[x][y] == 1 && (array[x - 1][y - 1] + array[x - 1][y] + array[x - 1][y + 1]   //Living with 2
-                + array[x][y - 1] + array[x][y + 1]
-                + array[x + 1][y - 1] + array[x + 1][y] + array[x + 1][y + 1]) == 2 ||
-                (array[x][y] == 1 && (array[x - 1][y - 1] + array[x - 1][y] + array[x - 1][y + 1]   //livin with 3
-                        + array[x][y - 1] + array[x][y + 1]
-                        + array[x + 1][y - 1] + array[x + 1][y] + array[x + 1][y + 1]) == 3))
-            return true;
-        else {
-            return false;
-        }
+    public static boolean happyCell(int[][] array, int x, int y) {
+        return (array[x][y] == 1 && (getLivingNeighbourCount(array, x, y)) == 2 ||
+                (array[x][y] == 1 && (getLivingNeighbourCount(array, x, y)) == 3));
     }
 
-    public static void Output(int[][] array) {
+    public static void output(int[][] array) {
         for (int column = 1; column < array.length - 1; column++) {
             for (int ro = 1; ro < array.length - 1; ro++) {
                 System.out.printf("%3d", array[column][ro]);
@@ -61,9 +47,9 @@ public class GameOfLife {
         }
     }
 
-    public static void GameOfLife(int[][] array, int[][] arrayNew, int playfield, int generations) {
+    public static void gameOfLife(int[][] array, int[][] arrayNew, int playfield, int generations) {
         for (int i = 0; i < generations; i++) {
-            Output(array);
+            output(array);
 
             System.out.println("---------------------");
 
@@ -72,11 +58,11 @@ public class GameOfLife {
 
 
                     //for 1/1 to max-1/max-1, 8 surrounding fields
-                    if (DeadCell(array, col, row) == true) {
+                    if (deadCell(array, col, row)) {
                         arrayNew[col][row] = 1;
-                    } else if (LonelyCell(array, col, row) == true) {
+                    } else if (lonelyCell(array, col, row)) {
                         arrayNew[col][row] = 0;
-                    } else if (HappyCell(array, col, row) == true) {
+                    } else if (happyCell(array, col, row)) {
                         arrayNew[col][row] = 1;
                     } else {
                         arrayNew[col][row] = 0;
@@ -94,16 +80,10 @@ public class GameOfLife {
         int[][] arrayNew = new int[fieldLength][fieldLength];
         int ticker = 10;       //USE CTRL - F2
         randomizeArray(array);
-        GameOfLife(array, arrayNew, fieldLength, ticker);
+        gameOfLife(array, arrayNew, fieldLength, ticker);
     }
 
 }
-
-//
-//(array[col][row] == 0 && (array[col - 1][row - 1] + array[col - 1][row] + array[col - 1][row + 1]   //Dead Cell with 3 living neighbours
-//        + array[col][row - 1] + array[col][row + 1]
-//        + array[col + 1][row - 1] + array[col + 1][row] + array[col + 1][row + 1]) == 3)
-
 
 
 
